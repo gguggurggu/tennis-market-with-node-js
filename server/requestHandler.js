b07bde02b5f30b7229a616f76354c06e66cda202
gguggurggu <sentmeroses121@gmail.com>
2025-02-05 15:15:29 +0900

    Adjusted all the error (still only Red Racket button is activated.)

--- server/requestHandler.js
+++ server/requestHandler.js
@@ -1,6 +1,7 @@
 //화면 불러오기 (file sync)
 const fs = require("fs");
 const mainView = fs.readFileSync("../main.html", "utf-8");
+const orderlistView = fs.readFileSync("../order-list.html", "utf-8");
 const mainCSS = fs.readFileSync("../main.css", "utf-8");
 
 //mariadb 불러오기
@@ -17,6 +18,50 @@ function main(response) {
   response.write(mainView);
   response.write("<style>" + mainCSS + "</style>");
   response.end();
+}
+
+//버튼 누를 때마다 orderlist에 행 추가
+function order(response, productId) {
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  mariadb.query(
+    "INSERT INTO orderlist VALUES (" +
+      productId +
+      ", '" +
+      new Date().toLocaleDateString() +
+      "');",
+    function (err, rows) {
+      console.log(rows);
+    }
+  );
+  response.write(
+    "Thank you for your order! <br> Please check the result on your order list page."
+  );
+  response.end;
+}
+
+//orderlist에 추가된 order 확인 가능
+function orderlist(response) {
+  console.log("orderlist");
+  response.writeHead(200, { "Content-Type": "text/html" });
+  mariadb.query("SELECT * FROM orderlist", function (err, rows) {
+    response.write(orderlistView);
+
+    rows.forEach((element) => {
+      response.write(
+        "<tr>" +
+          "<td>" +
+          element.product_id +
+          "</td>" +
+          "<td>" +
+          element.order_date +
+          "</td>" +
+          "</tr>"
+      );
+    });
+    response.write("</table>");
+    response.end();
+  });
 }
 
 function favicon() {
@@ -47,28 +92,11 @@ function blackRacket(response) {
   });
 }
 
-function order(response, productId) {
-  response.writeHead(200, { "Content-Type": "text/html" });
-
-  mariadb.query(
-    "INSERT INTO orderlist VALUES (" +
-      productId +
-      ", '" +
-      new Date().toLocaleDateString() +
-      "');",
-    function (err, rows) {
-      console.log(rows);
-    }
-  );
-
-  response.write("order page");
-  response.end();
-}
-
 // key:value
 let handle = {};
 handle["/"] = main;
 handle["/order"] = order;
+handle["/order-list.html"] = orderlist;
 handle["/favicon.ico"] = function () {};
 
 /* image directory */
